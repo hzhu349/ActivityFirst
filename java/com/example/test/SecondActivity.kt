8dd0d78843519ec67a7e1192c2ad3fd9ba650675
hzhu349 <85255321+hzhu349@users.noreply.github.com>
2021-06-23 15:05:06 -0700

    Add files via upload

---
+++ java/com/example/test/SecondActivity.kt
@@ -0,0 +1,65 @@
+package com.example.test
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        init()
+    }
+
+
+    private fun init() {
+        var fragmentManager = supportFragmentManager
+
+
+        var fragmentTransaction = fragmentManager.beginTransaction()
+        var blankFragment = BlankFragment()
+
+
+        button_add.setOnClickListener {
+
+
+            fragmentTransaction.add(R.id.fragment_container, blankFragment)
+            fragmentTransaction.commit()
+
+
+
+
+
+        }
+
+        button_drop.setOnClickListener {
+ /*           supportFragmentManager.beginTransaction().remove(R.id.blankFragment).commit()
+*/
+            fragmentTransaction.remove(blankFragment).commit()
+
+            var intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
+
+
+
+        }
+
+/*        supportFragmentManager.addOnBackStackChangedListener {
+
+
+            if (fragmentManager.backStackEntryCount() > 0)
+            { val lastFragment = fragmentManager.fragments.last()
+                fragmentTransaction.add(R.id.fragment_container,lastFragment)
+            }
+            else{
+                finish()
+            }
+
+            }*/
+        }
+
+
+
+}
